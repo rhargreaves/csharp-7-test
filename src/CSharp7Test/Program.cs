de7a9b821fa5580678867cfa7789633f166ef648
rhargreaves <kiyouta@gmail.com>
2017-08-01 19:19:52 +0100

    Test code

---
+++ src/CSharp7Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp7Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var o = Output();
+			Console.WriteLine($"{o.Item2} {o.Item1}");
+		}
+
+		static (int, string) Output()
+		{
+			return (7, "Hello from C Sharp ");
+		}
+	}
+}
