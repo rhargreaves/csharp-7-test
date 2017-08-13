ce3252552c6a3bea2b1fa9d42578b74dddfe6f05
rhargreaves <kiyouta@gmail.com>
2017-08-13 21:11:03 +0100

    Add CSharp 6 Test

---
+++ src/CSharp6Test/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSharp6Test
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Test()
+		{
+			var o = Output();
+			Console.WriteLine($"{o.Item2} {o.Item1}");
+		}
+
+		static Tuple<int, string> Output()
+		{
+			return Tuple.Create(6, "Hello from C Sharp ");
+		}
+	}
+}
